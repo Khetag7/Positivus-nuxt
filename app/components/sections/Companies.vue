082f23be4dd88ef090c350ede9de062249b2fd8d
Khetag <pubgccntgml@gmail.com>
2025-11-09 14:42:58 +0300

    new section Team finish

--- app/components/sections/Companies.vue
+++ app/components/sections/Companies.vue
@@ -5,13 +5,25 @@
 <template>
     <section class="companies">
       <div class="container g-flex company__logoes">
-
-          <img src="@/assets/images/logoes/CompanyLogo.png" alt="CompanyLogo">
-          <img src="@/assets/images/logoes/CompanyLogo2.png" alt="CompanyLogo2">
-          <img src="@/assets/images/logoes/CompanyLogo3.png" alt="CompanyLogo3">
-          <img src="@/assets/images/logoes/CompanyLogo4.png" alt="CompanyLogo4">
-          <img src="@/assets/images/logoes/CompanyLogo5.png" alt="CompanyLogo5">
-          <img src="@/assets/images/logoes/CompanyLogo6.png" alt="CompanyLogo6">
+        
+          <nuxt-link to="/">
+            <img src="@/assets/images/logoes/CompanyLogo.png" alt="CompanyLogo">
+          </nuxt-link>
+          <nuxt-link to="/">
+            <img src="@/assets/images/logoes/CompanyLogo2.png" alt="CompanyLogo2">
+          </nuxt-link>
+          <nuxt-link to="/">
+            <img src="@/assets/images/logoes/CompanyLogo3.png" alt="CompanyLogo3">
+          </nuxt-link>
+          <nuxt-link to="/">
+            <img src="@/assets/images/logoes/CompanyLogo4.png" alt="CompanyLogo4">
+          </nuxt-link>
+          <nuxt-link to="/">
+            <img src="@/assets/images/logoes/CompanyLogo5.png" alt="CompanyLogo5">
+          </nuxt-link>
+          <nuxt-link to="/">
+            <img src="@/assets/images/logoes/CompanyLogo6.png" alt="CompanyLogo6">
+          </nuxt-link>
 
       </div>
     </section>
---
+++ app/components/sections/Team.vue
@@ -0,0 +1,84 @@
+<script setup>
+import PictureTeam from '@/assets/images/Pictures/PictureTeam.png'
+import PictureTeam2 from '@/assets/images/Pictures/PictureTeam2.png'
+import PictureTeam3 from '@/assets/images/Pictures/PictureTeam3.png'
+import PictureTeam4 from '@/assets/images/Pictures/PictureTeam4.png'
+import PictureTeam5 from '@/assets/images/Pictures/PictureTeam5.png'
+import PictureTeam6 from '@/assets/images/Pictures/PictureTeam6.png'
+
+
+const gridContent = ref([
+{
+  picture: PictureTeam,
+  name: 'John Smith',
+  info: 'CEO and Founder',
+  text: '10+ years of experience in digital marketing. Expertise in SEO, PPC, and content strategy'
+},
+{
+  picture: PictureTeam2,
+  name: 'Jane Doe',
+  info: 'Director of Operations',
+  text: '7+ years of experience in project management and team leadership. Strong organizational and communication skills'
+},
+{
+  picture: PictureTeam3,
+  name: 'Michael Brown',
+  info: 'Senior SEO Specialist',
+  text: '5+ years of experience in SEO and content creation. Proficient in keyword research and on-page optimization'
+},
+{
+  picture: PictureTeam4,
+  name: 'Emily Johnson',
+  info: 'PPC Manager',
+  text: '3+ years of experience in paid search advertising. Skilled in campaign management and performance analysis'
+},
+{
+  picture: PictureTeam5,
+  name: 'Brian Williams',
+  info: 'Social Media Specialist',
+  text: '4+ years of experience in social media marketing. Proficient in creating and scheduling content, analyzing metrics, and building engagement'
+},
+{
+  picture: PictureTeam6,
+  name: 'Sarah Kim',
+  info: 'Content Creator',
+  text: '2+ years of experience in writing and editing Skilled in creating compelling, SEO-optimized content for various industries'
+}
+])
+</script>
+
+<template>
+  <section class="team">
+    <div class="container team-flex">
+      <div class="team__titles">
+        <h2 class="team__titles-title">Team</h2>
+        <p class="team__titles-text">
+          Meet the skilled and experienced team behind our successful digital marketing strategies
+        </p>
+      </div>
+      
+      <div class="team__grid">
+        <nuxt-link to="/" v-for="content in gridContent" :key="content.index" class="team__grid-block">
+          <div class="team__grid-block-top">
+            <div class="team__grid-block-top-texts">
+              <img :src="content.picture" alt="Picture">
+              <div class="team__grid-block-top-name">
+                <h4 class="team__grid-block-top-name-h4">{{ content.name }}</h4>
+                <p class="team__grid-block-top-name-p">{{ content.info }}</p>
+              </div>
+            </div>
+            <img src="@/assets/images/Icons/SocialIconTeam.png" alt="Icon" class="socialicon__img">
+          </div>
+          
+          <p class="team__grid-block-text">{{ content.text }}</p>
+        </nuxt-link>
+      </div>
+      
+      <nuxt-link to="/" class="team__btn">See all team</nuxt-link>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+@use '@/components/sections/Team.scss';
+</style>
